e0cb098f7fcef8c5526e29a1ad115daf3e3b6e9d
Thushan W A R T <rusiththarindu2@gmail.com>
2024-11-28 23:06:21 +0530

    feat: About Section'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import About from "./components/About"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      {/* <Navbar /> */}
       <Hero />
-      <section className="min-h-screen bg-blue-500"></section>
+      <About/>
+      <Footer/>
     </main>
   )
 }
