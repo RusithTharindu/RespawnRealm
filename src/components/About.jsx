de7328401f4f79ffac6d29a8335dc3359133f652
Thushan W A R T <rusiththarindu2@gmail.com>
2024-11-29 01:03:08 +0530

    feat: Animated Text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 
-// import AnimatedTitle from "./AnimatedTitle";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -33,14 +33,10 @@ const About = () => {
           Welcome to Respawn Realm
         </p>
 
-        {/* <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
-          containerClass="mt-5 !text-black text-center"
-        /> */}
-
-        <p className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-            Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-        </p>
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br/> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center" 
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins—your life, now an epic MMORPG</p>
